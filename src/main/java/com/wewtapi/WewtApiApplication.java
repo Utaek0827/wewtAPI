171c007d479fc9380c7f17d640c957c06e6281ca
utaek <utaek777@gmail.com>
2023-12-29 16:20:51 +0900

    .

---
+++ src/main/java/com/wewtapi/WewtApiApplication.java
@@ -0,0 +1,13 @@
+package com.wewtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WewtApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WewtApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wewtapi/WewtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wewtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WewtApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
