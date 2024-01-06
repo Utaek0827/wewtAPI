f3721f248f8b4a1935a7738ee0c3d66aadec11d8
utaek <utaek777@gmail.com>
2024-01-06 15:37:56 +0900

    Image upload

--- src/main/java/com/wewtapi/cons/MainCon.java
+++ src/main/java/com/wewtapi/cons/MainCon.java
@@ -1,7 +1,23 @@
 package com.wewtapi.cons;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Objects;
+
 
 @RestController
 public class MainCon {
@@ -13,6 +29,38 @@ public class MainCon {
         return "test";
     }
 
+    private final RestTemplate restTemplate;
+
+    public MainCon(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @PostMapping(value = "/file", consumes = {"multipart/form-data"})
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("title") String title) throws IOException {
+        File convFile = convertMultiPartToFile(file);
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", new FileSystemResource(convFile));
+        body.add("title", title);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8081/upload", requestEntity, String.class);
+
+        // 파일 삭제 처리
+        convFile.delete();
+
+        return response;
+    }
+
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
+    }
 
 
 }
